89ab658e7cd572bec4568a949fe9e60eceff0e05
Sherab Tenzing Lama <sherabtenzinglama@Sherabs-MacBook-Air.local>
2025-12-28 17:09:47 +0530

    minor chnages

--- qtify/src/components/hero.jsx
+++ qtify/src/components/hero.jsx
@@ -1,10 +1,12 @@
-import hero from "./assets/Hero.svg";
+import hero from "./assets/hero.svg"
 
 function Hero() {
     return(
         <div className="hero-container">
+            <div>
             <h1>100 Thousand Songs, ad-free</h1>
             <h1>Over thousands podcast episodes</h1>
+            </div>
             <img src={hero} className="hero-img" alt="hero"></img>
         </div>
 
